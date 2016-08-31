feda0c11b9a803a5b5126cfd7172be3d7d66b441
mvandriessen <Maarten.VanDriessen@realdolmen.com>
2016-08-31 14:39:30 +0200

    Add logging

--- Remove-Logs.ps1
+++ Remove-Logs.ps1
@@ -20,27 +20,40 @@ Function Remove-Logs{
 Param
 (
     [string]$FilePath,
-    [datetime]$Cutoff
+    [datetime]$Cutoff,
+    [string]$LogPath
 )
 
-    #Check if the path exists
+    #Check if the file & log path exist
     if(-not (Test-Path $FilePath))
     {
-        throw "Invalid path! Please make sure you enter a valid path."
+        throw "Invalid file path! Please make sure you enter a valid path."
+    }
+    elseif(-not (Test-Path $LogPath))
+    {
+        throw "Invalid log path! Please make sure you enter a valid path."
     }
 
+    #Get all items inside the path
     $LogFiles = Get-ChildItem -Path $FilePath | Where-Object{$_.LastWriteTime -lt $Cutoff}
+    $DeletedItems = @()
     if($LogFiles)
     {
         foreach($LogFile in $LogFiles)
         {
+            #Remove all items
+            $DeletedItems += $LogFile
             $LogFile | Remove-Item
             Write-Host "Removing file $LogFile" -ForegroundColor Green
+        }
+        if($LogPath)
+        {
+            $LogName = Get-Date -Format "yyyy-MM-dd"
+            $DeletedItems | select Name,creationtime,lastwritetime | Export-Csv -path "$LogPath\DeletedLogs - $logname.csv" -Delimiter ";" -NoTypeInformation
         }
     }
     else 
     {
         Write-Host "No files were found." -ForegroundColor Red
     }
-
 }
