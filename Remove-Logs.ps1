425836bc672243aeed5ac546722e370b0a66c914
mvandriessen <Maarten.VanDriessen@realdolmen.com>
2016-09-01 14:21:09 +0200

    Add optional parameter handling

--- Remove-Logs.ps1
+++ Remove-Logs.ps1
@@ -1,32 +1,46 @@
 #Requires -Version 3.0
 
-<# 
-==================================================================================================
-Microsoft PowerShell Source File
-NAME  :     Remove-Logs
-AUTHORS     :     Maarten Van Driessen, Realdolmen
-CREATED :   31/08/2016
-UPDATE      :     
+Function Remove-Logs{ 
 
-
-UPDATE in V1.0
-* Initial Version of the script
-==================================================================================================
+<#
+.SYNOPSIS
+  Delete old log files
+.DESCRIPTION
+  This function will delete all .log and .txt files older than a certain number of days.
+  You have the ability to specify log path, extension (log or txt) and path.
+.NOTES
+  Author:  Maarten Van Driessen
+.PARAMETER FilePath
+  Specify the path where you want to delete the logs from.
+.PARAMETER CutOff
+  Specify how many days you want to go back.
+.PARAMETER LogPath
+  Where do you want to store the log file. Path must end with a \
+.PARAMETER FileExtension
+  Specify whether you want to delete *.log or *.txt files. If left blank, the function will delete both.
+  Must be written as *.log or *.txt
+.EXAMPLE
+  Remove-Logs -FilePath C:\inetpub\ -Cutoff 30 -LogPath c:\temp\
+  
+  Delete all txt and log files older than 30 days from the c:\inetpub folder and write the log to c:\reports
+.EXAMPLE
+  Remove-Logs -FilePath C:\inetpub\ -Cutoff 20 -LogPath c:\temp\ -Filter *.txt
+  
+  Delete all txt files older than 20 days from the c:\inetpub folder and write the log to c:\reports
 #>
 
-Function Remove-Logs{ 
 [Cmdletbinding()]
 #Parameters
 Param
 (
     #Check to see if there are any invalid characters in the path
-    [ValidateScript({
+    [Parameter(Mandatory=$true)][ValidateScript({
             If ((Split-Path $_ -Leaf).IndexOfAny([io.path]::GetInvalidFileNameChars()) -ge 0) {
                 Throw "$(Split-Path $_ -Leaf) contains invalid characters!"
             } Else {$True}
         })][string]$FilePath,
     #You can only delete logs older than 1 day
-    [ValidateRange(1,365)][int]$Cutoff,
+    [Parameter(Mandatory=$true)][ValidateRange(1,365)][int]$Cutoff,
     #Check to see if there are any invalid characters in the path
     [ValidateScript({
             If ((Split-Path $_ -Leaf).IndexOfAny([io.path]::GetInvalidFileNameChars()) -ge 0) {
@@ -34,8 +48,16 @@ Param
             } Elseif(-Not ($_.EndsWith('\'))){
                 Throw "Logpath must end with \ !"
             } Else {$True}
+        })]$LogPath="",  
+    
+    <#[ValidateScript({
+           If((Split-Path $_ -Leaf).IndexOfAny([io.path]::GetInvalidFileNameChars()) -ge 0) {
+                Throw "$(Split-Path $_ -Leaf) contains invalid characters!"
+            } Elseif(-Not ($_.EndsWith('\'))){
+                Throw "Logpath must end with \ !"
+            } Else {write-host $_ $True}
         })]
-    [string]$LogPath,
+    [string]$LogPath,#>
     #you can only delete log and txt files
     [ValidateSet("*.log","*.txt")][string]$FileExtension ="*.*"
     
@@ -46,12 +68,13 @@ Param
     {
         throw "Invalid file path! Please make sure you enter a valid path."
     }
+    #If statement to handle optional parameter
+    elseif($LogPath -eq "") {}
     elseif(-not (Test-Path $LogPath))
     {
-        throw "Invalid log path! Please make sure you enter a valid path."
+        throw "Log path does not exist! Please make sure you enter a valid path."
     }
 
-    #
     $CutOffDate = (Get-Date).AddDays(-$Cutoff)
 
     #Get all items inside the path
